bd811a413905bbe0c7f4bb2d010a4ee486df4e2a
Louie Alfonso <lfriasalfonso@gmail.com>
2025-08-01 20:44:13 +0100

    Install Boostrap 5 & Created Templates for Master & Public View

--- django_warehouse/django_warehouse/urls.py
+++ django_warehouse/django_warehouse/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from public.views import public
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', public, name='public')
 ]
