5536378e048073053bdb967c862a4e7578b2a6f2
Louie Alfonso <lfriasalfonso@gmail.com>
2025-08-06 20:37:55 +0100

    Update Main Template & Model, Views, Forms with  Urls for Product

--- django_warehouse/product/models.py
+++ django_warehouse/product/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    category_code = models.CharField(max_length=255)
+    category_status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.category_code} - {self.category_name}"
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=255)
@@ -9,9 +21,11 @@ class Product(models.Model):
     sku = models.CharField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    #category = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.id} - {self.product_name}"
+
+
--- django_warehouse/product/views.py
+++ django_warehouse/product/views.py
@@ -2,9 +2,44 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Product
 
+# Display Product Lists
 def product_lists(request):
-    return render(request, "product/product_list.html",{"products": Product.objects.all()})
+    return render(request, "product/product-list.html",{"products": Product.objects.all()})
+
+# Add New Product
+def add_product(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("product/product-lists")
+    else:
+        form = ProductForm()
+    return render(request, "product/add-product.html", {"form": form}) 
+
+# Display Product Details
+def product_details(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, "product/product-detail.html", {"product": product})
 
 
+# Edit Product
+def edit_produt(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("product/product-details", id)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, "product/edit-product.html", {"form": form})
 
-
+# Delete Product
+def delete_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == "POST":
+        meeting.delete()
+        return redirect("product/product-lists")
+    else:
+        return render(request, "product/delete-product.html", {"product": product})
