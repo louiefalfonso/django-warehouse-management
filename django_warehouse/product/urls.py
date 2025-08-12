2476e27585372fd2adca92df7e5db164337ec95e
Louie Alfonso <lfriasalfonso@gmail.com>
2025-08-12 20:43:52 +0100

    Added Warehouse App, Update Models, Views & Urls for Products

--- django_warehouse/product/urls.py
+++ django_warehouse/product/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_lists, name='product-lists'),
+    path('', views.product_lists, name='products'),
     path('add-product', views.add_product, name="add-product"),
+    path('<int:sid>', views.product_details, name="product-details"),
+    path('edit/<int:id>', views.edit_product, name="edit-product"),
+    path('delete/<int:id>', views.delete_product, name="delete-product"),
 ]
